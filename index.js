3b8bcb0677b776e5d6f44a0af8982fd6dbe66c1e
Mamta sharma <your_email@example.com>
2023-05-27 10:34:35 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const mobileNavbarButton = document.querySelector(".mobile-navbar-button");
+const header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+    header.classList.toggle("active");
+}
+
+mobileNavbarButton.addEventListener("click", toggleNavbar);
