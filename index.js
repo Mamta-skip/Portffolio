cc2f008e814ba5be877f75e10f6849691c8b70b8
Mamta sharma <your_email@example.com>
2023-05-29 01:11:17 -0700

    addition of buttom to top button

--- index.js
+++ index.js
@@ -6,3 +6,36 @@ const toggleNavbar = () => {
 }
 
 mobileNavbarButton.addEventListener("click", toggleNavbar);
+
+
+
+
+
+
+
+// js for back to top button starts here
+//Get the button
+var backToTopBtn = document.getElementById("buttonTopArrow");
+
+// show the button, when scrolled down 40px from the top 
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 40 ||
+    document.documentElement.scrollTop > 40
+  ) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+//scroll to the top, when clicked on the button, 
+backToTopBtn.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
